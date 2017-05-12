2f0f6603a3c08c50ba728a218a5e94c9f6da22a3
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-12 14:42:47 +0200

    Export http client

--- declarations/chupacabras.d.ts
+++ declarations/chupacabras.d.ts
@@ -13,6 +13,10 @@ export interface IModelRetriever {
 }
 
 export class ModelRetriever implements IModelRetriever {
+
+    constructor(httpClient: IHttpClient, notificationManager: INotificationManager,
+                parametersDeserializer: IParametersDeserializer);
+
     modelFor<T>(context: ModelContext): Observable<T>;
 }
 
@@ -33,4 +37,23 @@ interface Notification {
 
 export interface IParametersDeserializer {
     deserialize(context: ModelContext): {};
+}
+
+export interface IHttpClient {
+    get(url: string, headers?: Dictionary<string>): Observable<HttpResponse>
+}
+
+export class HttpClient implements IHttpClient {
+
+    get(url: string, headers?: Dictionary<string>): Observable<HttpResponse>;
+}
+
+export class HttpResponse<T> {
+    response: T;
+    status: number;
+    headers: Dictionary<string>;
+}
+
+interface Dictionary<T> {
+    [index: string]: T
 }
