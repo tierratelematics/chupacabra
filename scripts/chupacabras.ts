e11b51101ef6b9bee9e0f994d9bd433a3e7dad24
Mattia Franchetto <mt.franchetto@gmail.com>
2017-05-12 14:47:54 +0200

    Export http client constructor

--- scripts/chupacabras.ts
+++ scripts/chupacabras.ts
@@ -1,3 +1,4 @@
 export {default as ModelRetriever} from "./model/ModelRetriever";
 export {default as NotificationManager} from "./notifications/NotificationManager";
 export {default as ModelContext} from "./model/ModelContext";
+export {default as HttpClient} from "./net/HttpClient";
