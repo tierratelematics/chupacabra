c76114074640b2170858f13268b02e5e6de3bd26
Mattia Franchetto <mt.franchetto@gmail.com>
2017-06-06 17:59:54 +0200

    Change parameters deserializer signature

--- scripts/model/ParametersDeserializer.ts
+++ scripts/model/ParametersDeserializer.ts
@@ -1,12 +1,12 @@
 import ModelContext from "./ModelContext";
 
 export interface IParametersDeserializer {
-    deserialize(context: ModelContext): {};
+    deserialize(context: ModelContext): object;
 }
 
 export class NullParametersDeserializer implements IParametersDeserializer {
 
-    deserialize(context: ModelContext): {} {
+    deserialize(context: ModelContext): object {
         return null;
     }
 
