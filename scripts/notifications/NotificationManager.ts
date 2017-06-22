7f8293678e39695dd4bcce8564338f3920919c76
Francesco Pirrotta <fpirrotta@topcon.com>
2017-06-22 17:07:47 +0200

    Prevent channel unsubscribtion on multiple context subscription

--- scripts/notifications/NotificationManager.ts
+++ scripts/notifications/NotificationManager.ts
@@ -1,13 +1,13 @@
 import INotificationManager from "./INotificationManager";
 import Notification from "./Notification";
-import {Observable} from "rx";
+import { Observable } from "rx";
 import ModelContext from "../model/ModelContext";
 
 class NotificationManager implements INotificationManager {
 
-    constructor(private client: SocketIOClient.Socket) {
+    private refCounts: { [key: string]: number } = {};
 
-    }
+    constructor(private client: SocketIOClient.Socket) { }
 
     notificationsFor(context: ModelContext): Observable<Notification> {
         this.subscribeToChannel(context);
@@ -15,15 +15,21 @@ class NotificationManager implements INotificationManager {
     }
 
     protected getNotificationStream(context: ModelContext): Observable<Notification> {
-        return Observable.fromEvent<Notification>(this.client, `${context.area}:${context.modelId}`);
+        return Observable.fromEvent<Notification>(this.client, keyFor(context));
     }
 
     private subscribeToChannel(context: ModelContext): void {
-        this.operateOnChannel('subscribe', context);
+        this.operateOnChannel("subscribe", context);
+        if (!this.refCounts[keyFor(context)]) this.refCounts[keyFor(context)] = 1;
+        else this.refCounts[keyFor(context)]++;
     }
 
     private unsubscribeFromChannel(context: ModelContext): void {
-        this.operateOnChannel('unsubscribe', context);
+        this.refCounts[keyFor(context)]--;
+        if (this.refCounts[keyFor(context)] > 0) return;
+
+        this.operateOnChannel("unsubscribe", context);
+        delete this.refCounts[keyFor(context)];
     }
 
     private operateOnChannel(operation: string, context: ModelContext): void {
@@ -31,4 +37,8 @@ class NotificationManager implements INotificationManager {
     }
 }
 
-export default NotificationManager
+function keyFor(context: ModelContext): string {
+    return `${context.area}:${context.modelId}`;
+}
+
+export default NotificationManager;
--- test/NotificationManagerSpec.ts
+++ test/NotificationManagerSpec.ts
@@ -1,5 +1,5 @@
 import expect = require("expect.js");
-import {IMock, Mock, Times, It} from "typemoq"
+import { IMock, Mock, Times, It } from "typemoq"
 import NotificationManager from "../scripts/notifications/NotificationManager";
 import ModelContext from "../scripts/model/ModelContext";
 
@@ -17,24 +17,28 @@ describe("NotificationManager, given an area and a model id", () => {
         it("should subscribe to the backend", () => {
             subject.notificationsFor(new ModelContext("Admin", "FakePage"));
 
-            client.verify(c => c.emit('subscribe', It.isValue(new ModelContext("Admin", "FakePage"))), Times.once());
+            client.verify(c => c.emit("subscribe", It.isValue(new ModelContext("Admin", "FakePage"))), Times.once());
         });
 
         context("and custom parameters are needed on the backend side", () => {
             it("should also add these parameters to the subscription request", () => {
-                subject.notificationsFor(new ModelContext("Admin", "FakePage", {id: 60}));
+                subject.notificationsFor(new ModelContext("Admin", "FakePage", { id: 60 }));
 
-                client.verify(c => c.emit('subscribe', It.isValue(new ModelContext("Admin", "FakePage", {id: 60}))), Times.once());
+                client.verify(c => c.emit("subscribe", It.isValue(new ModelContext("Admin", "FakePage", { id: 60 }))), Times.once());
             });
         });
     });
 
-    context("when a notifications is not needed anymore", () => {
+    context("when all notifications are not needed anymore", () => {
         it("should dispose the subscription", () => {
-            let subscription = subject.notificationsFor(new ModelContext("Admin", "FakePage", {id: 60})).subscribe();
+            let subscription = subject.notificationsFor(new ModelContext("Admin", "FakePage", { id: 60 })).subscribe();
+            let anotherSubscription = subject.notificationsFor(new ModelContext("Admin", "FakePage", { id: 60 })).subscribe();
             subscription.dispose();
 
-            client.verify(c => c.emit('unsubscribe', It.isValue(new ModelContext("Admin", "FakePage", {id: 60}))), Times.once());
+            client.verify(c => c.emit("unsubscribe", It.isValue(new ModelContext("Admin", "FakePage", { id: 60 }))), Times.never());
+
+            anotherSubscription.dispose();
+            client.verify(c => c.emit("unsubscribe", It.isValue(new ModelContext("Admin", "FakePage", { id: 60 }))), Times.once());
         });
     });
 });
